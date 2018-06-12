8437d8b854f6c0e92a306331e142e9588a3a47f8
wpixel <wpixel@163.com>
2018-06-12 16:48:54 +0800

    添加图片

---
+++ src/com/wpixel/listener/TopMenuListener.java
@@ -0,0 +1,50 @@
+package com.wpixel.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.wpixel.main.Client;
+import com.wpixel.ui.ClientUI;
+
+/**
+ * 顶部菜单按钮事件
+ * @author RWBY
+ *
+ */
+public class TopMenuListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+//		// 创建一个模态对话框
+//        final JDialog dialog = new JDialog(ClientUI.frame, "提示", true);
+//        // 设置对话框的宽高
+//        dialog.setSize(600, 400);
+//        // 设置对话框大小不可改变
+//        dialog.setResizable(false);
+//        // 设置对话框相对显示的位置
+//        dialog.setLocationRelativeTo(ClientUI.frame);
+//        // 创建一个标签显示消息内容
+//        JLabel messageLabel = new JLabel("对话框消息内容");
+//        // 创建一个按钮用于关闭对话框
+//        JButton okBtn = new JButton("确定");
+//        okBtn.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                //关闭对话框
+//                dialog.dispose();
+//            }
+//        });
+//        // 创建对话框的内容面板, 在面板内可以根据自己的需要添加任何组件并做任意是布局
+//        JPanel panel = new JPanel();
+//        // 添加组件到面板
+//        panel.add(messageLabel);
+//        panel.add(okBtn);
+//        // 设置对话框的内容面板
+//        dialog.setContentPane(panel);
+//        // 显示对话框
+//        dialog.setVisible(true);
+	}
+}
